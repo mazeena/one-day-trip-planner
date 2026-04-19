3ce83b2b02f3c55ddc00549cd253381f928262d4
mazeena <mazeenaalavi@gmail.com>
2026-04-19 09:05:19 +0530

    Add Trip model and migrations

---
+++ app/Models/Trip.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Trip extends Model
+{
+    protected $fillable = ['user_id', 'trip_name', 'trip_date'];
+
+    public function attractions()
+    {
+        return $this->belongsToMany(
+            Attraction::class,
+            'trip_attractions',
+            'trip_id',
+            'attraction_id'
+        );
+    }
+}
